58c994935de3960a1e565d3030c0144098035601
Isaac <otichinazaisaac19@gmail.com>
2022-06-17 15:01:02 +0100

    done the desktop screen

---
+++ app.js
@@ -0,0 +1,55 @@
+// function scrollToTop() {
+//   window.scrollTo({
+//     behavior: "smooth",
+//   });
+// }
+
+document.addEventListener("scroll", () => {
+  let scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    navC.style.backgroundColor = "rgba(88, 88, 88, .6)";
+  } else {
+    navC.style.backgroundColor = "transparent";
+  }
+});
+
+// hamburger
+
+const hamburger = document.querySelector(".hamburger-bars");
+const navC = document.querySelector(".nav");
+const navlinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", (item) => {
+  hamburger.classList.toggle("active");
+  navlinks.classList.toggle("active");
+  console.log(item);
+  // mobile_menu.classList.toggle("active");
+});
+
+const pageLoader = document.querySelector(
+  ".loadingio-spinner-dual-ball-f4bxwls9top"
+);
+
+window.addEventListener("load", () => {
+  pageLoader.style.display = "none";
+});
+
+// intersection observer
+let items = document.querySelectorAll(".services-item-content");
+
+let Observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((e) => {
+      e.target.classList.toggle("show", e.isIntersecting);
+      if (e.isIntersecting) {
+        Observer.unobserve(e.target);
+      }
+    });
+  },
+  {
+    threshold: 0,
+  }
+);
+items.forEach((i) => {
+  Observer.observe(i);
+});
